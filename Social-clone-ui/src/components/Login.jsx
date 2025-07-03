cfaec16c84ec36e2613b9c7752f0c234347c93fe
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-03 06:32:54 +0000

    sign up and log in flow works perfectly

--- Social-clone-ui/src/components/Login.jsx
+++ Social-clone-ui/src/components/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('https://localhost:8080/users/login', {
+            const response = await axios.post('http://localhost:8080/users/login', {
                 email,
                 password
             });
