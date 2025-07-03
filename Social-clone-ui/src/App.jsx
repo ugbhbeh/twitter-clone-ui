d3641cc8dc7c8eb5ecb96995a0236b2781556bc4
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-03 04:23:58 +0000

    added routing to compotents

--- Social-clone-ui/src/App.jsx
+++ Social-clone-ui/src/App.jsx
@@ -1,14 +1,29 @@
-
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
- 
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+
+  useEffect(() => {
+    const handleStorage = () => {
+      setIsAuthenticated(!!localStorage.getItem('token'));
+    };
+    window.addEventListener('storage', handleStorage);
+    return () => window.removeEventListener('storage', handleStorage);
+  }, []);
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Route>
+        <Route path="/" element={<Home isAuthenticated={isAuthenticated} /> } />
+        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to ="/" /> } />
+        <Route path="/signup" element={!isAuthenticated ? <Signup /> : <Navigate to ="/" /> } />
+        </Route>
+    </Router>
   )
 }
 
