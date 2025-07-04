a96c87cf61e028e0d25f0f5344010380c94a36a5
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-04 04:00:12 +0000

    project structuring

--- Social-clone-ui/src/App.jsx
+++ Social-clone-ui/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './components/Home'
+import Home from './pages/Home'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import { useState, useEffect } from 'react';
---
+++ Social-clone-ui/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import TopBar from '../components/TopBar';
+import PostFeed from '../components/PostFeed';
+import CreatePost from '../components/CreatePost';
+
+function Home() {
+<></>
+}
