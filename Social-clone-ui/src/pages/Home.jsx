b688d841793815ab8a0b1cda77db50ad4fe6933e
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-04 04:09:02 +0000

    home page skeleton done

--- Social-clone-ui/src/pages/Home.jsx
+++ Social-clone-ui/src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import TopBar from '../components/TopBar';
 import PostFeed from '../components/PostFeed';
 import CreatePost from '../components/CreatePost';
+import SideBar from '../components/SideBar'
 
 function Home() {
-<></>
+return (
+    <div className='app-layout'>
+        <TopBar/>
+
+        <div className='main-content'>
+            <SideBar/>
+
+            <div className='feed'>
+                <CreatePost/>
+                 <PostFeed/>
+                 
+                </div>
+            </div>
+        </div>
+)
 }
+
+export default Home;
