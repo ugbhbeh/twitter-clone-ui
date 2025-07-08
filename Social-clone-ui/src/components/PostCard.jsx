9b3ce2a4e003d8fcd5f2c312be616f5b9010f636
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-08 04:18:33 +0000

    modified post feed to show likes, added helped functions

--- Social-clone-ui/src/components/PostCard.jsx
+++ Social-clone-ui/src/components/PostCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function PostCard({post, onLike, onDislike}) {
     const likeCount = post._count?.likes ?? 0;
-    const disLikeCount = post._count?.dislikes ?? 0;
+    const dislikeCount = post._count?.dislikes ?? 0;
     
     return (
         <div>
@@ -16,7 +16,7 @@ export default function PostCard({post, onLike, onDislike}) {
 
          <div>
             <button onClick={() => onLike?.(post.id)}> 👍 {likeCount} </button>
-            <button onClick={() => onDislike?.(post.id)}> 👍 {disLikeCount}  </button>
+            <button onClick={() => onDislike?.(post.id)}> 👍 {dislikeCount}  </button>
          </div>        
         </div>
 )
--- Social-clone-ui/src/components/PostFeed.jsx
+++ Social-clone-ui/src/components/PostFeed.jsx
@@ -6,9 +6,7 @@ export default function PostFeed() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
+const fetchPosts = async () => {
       setError('');
       setLoading(true);
       try {
@@ -21,9 +19,29 @@ export default function PostFeed() {
         setLoading(false);
       }
     };
-
+  
+    useEffect(() => {
     fetchPosts();
   }, []);
+
+  const handleLike = async (postId) => {
+    try {
+      await api.post(`/posts/${postId}/like`);
+      fetchPosts();
+      
+    } catch (err) {
+      console.error("Failed to like post", err)
+    }
+  };
+
+  const handleDislike = async (postId) => {
+    try {
+      await api.post(`/posts/${postId}/dislike`);
+      fetchPosts();
+    } catch (err){
+      console.log("Failed to dislike post", err)
+    }
+  };
 
   if (loading) return <div>Loading posts...</div>;
 
@@ -35,7 +53,7 @@ export default function PostFeed() {
         <p>No posts available.</p>
       ) : (
             posts.map(post => (
-              <PostCard key={post.id} post={post} />
+              <PostCard key={post.id} post={post} onLike={handleLike} onDislike={handleDislike} />
       ))
     )}
     </div>   
