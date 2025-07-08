fbca3bfd79ea6c0487993494e764799f5256e1b8
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-08 04:08:16 +0000

    added like and dislike ability to the postcard

--- Social-clone-ui/src/components/PostCard.jsx
+++ Social-clone-ui/src/components/PostCard.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 
 
-export default function PostCard({post}) {
+export default function PostCard({post, onLike, onDislike}) {
+    const likeCount = post._count?.likes ?? 0;
+    const disLikeCount = post._count?.dislikes ?? 0;
+    
     return (
         <div>
             <Link to={`/posts/${post.id}`}> 
@@ -9,7 +12,12 @@ export default function PostCard({post}) {
                 By {post.author?.username} . {new Date(post.createdAt).toLocaleString()}
             </p>
             <p>{post.content}</p>
-            </Link>         
+            </Link> 
+
+         <div>
+            <button onClick={() => onLike?.(post.id)}> 👍 {likeCount} </button>
+            <button onClick={() => onDislike?.(post.id)}> 👍 {disLikeCount}  </button>
+         </div>        
         </div>
 )
     
