65a8c897b0e775cb7ced1f7d78264e06aef8505b
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-16 05:03:48 +0000

    added routing from post feed

--- Social-clone-ui/src/components/PostCard.jsx
+++ Social-clone-ui/src/components/PostCard.jsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
 
+export default function PostCard({ post, onLike, onDislike }) {
+  const likeCount = post._count?.likes ?? 0;
+  const dislikeCount = post._count?.dislikes ?? 0;
 
-export default function PostCard({post, onLike, onDislike}) {
-    const likeCount = post._count?.likes ?? 0;
-    const dislikeCount = post._count?.dislikes ?? 0;
-    
-    return (
-        <div>
-            <Link to={`/posts/${post.id}`}> 
-            <p>
-                By {post.author?.username} . {new Date(post.createdAt).toLocaleString()}
-            </p>
-            <p>{post.content}</p>
-            </Link> 
+  return (
+    <div>
+      <div>
+        By{" "}
+        <Link to={`/profile/${post.author?.id}`}>
+          {post.author?.username || "Unknown"}
+        </Link>{" "}
+        · {new Date(post.createdAt).toLocaleString()}
+      </div>
+      <Link to={`/posts/${post.id}`}>
+        <p>{post.content}</p>
+      </Link>
 
-         <div>
-            <button onClick={() => onLike?.(post.id)}> 👍 {likeCount} </button>
-            <button onClick={() => onDislike?.(post.id)}> 👎 {dislikeCount}  </button>
-         </div>        
-        </div>
-)
-    
+      <div>
+        <button onClick={() => onLike?.(post.id)}>👍 {likeCount}</button>
+        <button onClick={() => onDislike?.(post.id)}>👎 {dislikeCount}</button>
+      </div>
+    </div>
+  );
 }
-
