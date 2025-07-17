ca4bcedf3f662ff26fa08d6360cebd24da5b042d
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-17 05:45:41 +0000

    both side bar elements work

--- Social-clone-ui/src/components/SideBar.jsx
+++ Social-clone-ui/src/components/SideBar.jsx
@@ -4,6 +4,7 @@ import MostPopularPosts from "./MostPopularPosts"
 export default  function SideBar() {
     return (
     <aside>
+        <MostFollowedUsers/>
       <MostPopularPosts />
     </aside>
   );
