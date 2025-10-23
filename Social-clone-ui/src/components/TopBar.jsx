3b8fece237eac5ed0274b1fa8a22a51a75cf6303
root <187004982+ugbhbeh@users.noreply.github.com>
2025-10-23 04:07:50 +0000

    got a cute little toggle for dark and light mode

--- Social-clone-ui/src/components/TopBar.jsx
+++ Social-clone-ui/src/components/TopBar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import AuthContext from "../services/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-import Explore from "../pages/Explore";
+import ThemeToggle from "./ThemeToggle";
 
 export default function TopBar() {
   const { isLoggedIn, logout } = useContext(AuthContext);
@@ -26,32 +26,73 @@ export default function TopBar() {
     <nav className="bg-surface shadow-md">
       <div className="container-app py-4">
         <div className="flex items-center justify-between">
+          {/* Left section */}
           <div className="flex items-center space-x-4">
-            <button onClick={handlehome} className="btn btn-outline btn-primary flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+            <button
+              onClick={handlehome}
+              className="btn btn-outline btn-primary flex items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
               </svg>
               Home
             </button>
-            <button onClick={handleExplore} className="btn btn-outline btn-primary flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M9.243 3.03a1 1 0 01.727 1.213L9.53 6h2.94l.56-2.243a1 1 0 111.94.486L14.53 6H17a1 1 0 110 2h-2.97l-1 4H15a1 1 0 110 2h-2.47l-.56 2.242a1 1 0 11-1.94-.485L10.47 14H7.53l-.56 2.242a1 1 0 11-1.94-.485L5.47 14H3a1 1 0 110-2h2.97l1-4H5a1 1 0 110-2h2.47l.56-2.243a1 1 0 011.213-.727zM9.03 8l-1 4h2.938l1-4H9.031z" clipRule="evenodd" />
+
+            <button
+              onClick={handleExplore}
+              className="btn btn-outline btn-primary flex items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-1"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.243 3.03a1 1 0 01.727 1.213L9.53 6h2.94l.56-2.243a1 1 0 111.94.486L14.53 6H17a1 1 0 110 2h-2.97l-1 4H15a1 1 0 110 2h-2.47l-.56 2.242a1 1 0 11-1.94-.485L10.47 14H7.53l-.56 2.242a1 1 0 11-1.94-.485L5.47 14H3a1 1 0 110-2h2.97l1-4H5a1 1 0 110-2h2.47l.56-2.243a1 1 0 011.213-.727zM9.03 8l-1 4h2.938l1-4H9.031z"
+                  clipRule="evenodd"
+                />
               </svg>
               Explore
             </button>
           </div>
-          <div className="flex items-center space-x-4">
+
+          {/* Right section */}
+          <div className="flex items-center space-x-3">
+            {/* Theme toggle positioned just before profile/login */}
+            <ThemeToggle />
+
             {isLoggedIn ? (
               <>
                 {userId && (
-                  <Link to={`/profile/${userId}`} className="btn btn-outline btn-secondary flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                  <Link
+                    to={`/profile/${userId}`}
+                    className="btn btn-outline btn-secondary flex items-center"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 mr-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clipRule="evenodd"
+                      />
                     </svg>
                     Profile
                   </Link>
                 )}
-                <button onClick={handleLogout} className="btn btn-secondary">Logout</button>
+                <button onClick={handleLogout} className="btn btn-secondary">
+                  Logout
+                </button>
               </>
             ) : (
               <Link to="/login">
