365e8c1a6df56a7e94b1c21cafdb4c13559af11c
root <187004982+ugbhbeh@users.noreply.github.com>
2026-04-17 04:34:35 +0000

    minor improvment

--- Social-clone-ui/src/pages/Home.jsx
+++ Social-clone-ui/src/pages/Home.jsx
@@ -5,8 +5,9 @@ import SideBar from '../components/SideBar'
 function Home() {
   return (
     <div className="flex gap-8 max-w-7xl mx-auto px-4 py-8">
-      <div className="flex-1 min-w-0">
+      <div className="flex-1 min-w-0 ">
         <CreatePost />
+        <br />
         <PostFeed />
       </div>
       <SideBar />
